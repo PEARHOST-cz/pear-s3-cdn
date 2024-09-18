584d639def8b139925bf894da2a7dbbdf54ce3d0
HrustakV <hrustakvcz@gmail.com>
2024-09-18 10:17:43 +0200

    debug/release option for gin

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{"message": "CDN OK"})
+		c.JSON(http.StatusOK, gin.H{"message": "CDN OK"})
 	})
 
 	accessKey := os.Getenv("MinIOAccessKey")
